127c09890ef32da3e10261c270b1fd243de1ea33
y-c-y-k-a <183938902+y-c-y-k-a@users.noreply.github.com>
2026-06-05 22:30:27 +0400

    Complete removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+  const result = arr.slice();
+
+  for (let i = 0; i < toRemove.length; i++) {
+
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === toRemove[i]) {
+        result.splice(j, 1);
+        j--;
+      }
+    }
+    
+  }
+
+  return result;
 };
 
 // Do not edit below this line
